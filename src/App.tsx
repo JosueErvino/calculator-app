302c033b1fef7150f4f1cb78c51f3f89e2729921
Josué <josue.feldeckircher@cwi.com.br>
2023-01-03 18:19:05 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/JosueErvino">Josué Ervino</a>.
+      </div>
+    </div>
+  )
+}
+
+export default App
